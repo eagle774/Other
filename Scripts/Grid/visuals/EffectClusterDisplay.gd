368bb4aaa4e2cba9401ebfa7de13204cbf53283d
eagle774 <38058650+eagle774@users.noreply.github.com>
2025-04-26 20:30:01 +1000

    Created Effect Cluster prefab

--- Scripts/Grid/visuals/EffectClusterDisplay.gd
+++ Scripts/Grid/visuals/EffectClusterDisplay.gd
@@ -1,2 +1,9 @@
 extends Node
-class_name ActionDisplay
+class_name EffectClusterDisplay
+
+# Should pair with a premade prefab to show an effect cluster
+# And all effects in it, as well as an option to confirm
+
+
+# All effects need a prefab for what they look like
+# And code to replace some blanks e.g. replace 0/0 hp with x/y hp
